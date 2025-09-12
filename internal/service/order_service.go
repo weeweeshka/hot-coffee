2537a2501ddb8790600479eaed5d922a04a8675d
weeweeshka <tvbkmuthfcbvjd2008@gmail.com>
2025-09-12 19:02:31 +0300

    create a half of order service layer

--- internal/service/order_service.go
+++ internal/service/order_service.go
@@ -12,6 +12,11 @@ type OrderImpl struct {
 }
 
 type OrderRepo interface {
+	Save(ctx context.Context, data models.Order) (string, error)
+	GetAll(ctx context.Context) ([]models.Order, error)
+	Get(ctx context.Context, id string) (models.Order, error)
+	Update(ctx context.Context, id string, order models.Order) (models.Order, error)
+	Delete(ctx context.Context, id string) error
 }
 
 func NewOrderService(repo OrderRepo, logr *slog.Logger) *OrderImpl {
@@ -19,4 +24,48 @@ func NewOrderService(repo OrderRepo, logr *slog.Logger) *OrderImpl {
 }
 
 func (o *OrderImpl) CreateOrder(ctx context.Context, data models.Order) (string, error) {
+
+	id, err := o.repo.Save(ctx, data)
+	if err != nil {
+		o.logr.Info("Failed to save order", "err", err)
+	}
+	return id, err
+}
+
+func (o *OrderImpl) GetOrders(ctx context.Context) ([]models.Order, error) {
+
+	var orders []models.Order
+	orders, err := o.repo.GetAll(ctx)
+	if err != nil {
+		o.logr.Info("Failed to get all orders", "err", err)
+	}
+	return orders, err
+}
+
+func (o *OrderImpl) GetOrder(ctx context.Context, id string) (models.Order, error) {
+	var order models.Order
+	order, err := o.repo.Get(ctx, id)
+	if err != nil {
+		o.logr.Info("Failed to get order", "err", err)
+	}
+	return order, err
+}
+
+func (o *OrderImpl) UpdateOrder(ctx context.Context, id string, order models.Order) (models.Order, error) {
+
+	nOrder, err := o.repo.Update(ctx, id, order)
+	if err != nil {
+		o.logr.Info("Failed to update order", "err", err)
+	}
+	return nOrder, err
+}
+
+func (o *OrderImpl) DeleteOrder(ctx context.Context, id string) error {
+
+	err := o.repo.Delete(ctx, id)
+	if err != nil {
+		o.logr.Info("Failed to delete order", "err", err)
+
+	}
+	return err
 }
