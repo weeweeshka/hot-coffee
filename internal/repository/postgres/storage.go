94884029717da1083b5119127404c39f0725cd76
weeweeshka <tvbkmuthfcbvjd2008@gmail.com>
2025-10-03 18:54:10 +0300

    changing data layer

--- internal/repository/postgres/storage.go
+++ internal/repository/postgres/storage.go
@@ -30,18 +30,85 @@ func NewStorage(logr *slog.Logger, connString string) (*Storage, error) {
 		db:   conn}, nil
 }
 
-func (s *Storage) SaveOrder(ctx context.Context, data models.OrderRequest) (int64, error) {
-
-	tx, err := o.db.Begin(ctx)
+func (s *Storage) SaveInventory(ctx context.Context, data models.InventoryItem) (int64, error) {
+	tx, err := s.db.Begin(ctx)
 	if err != nil {
 		return 0, fmt.Errorf("cannot begin transaction: %v", err)
 	}
-
 	defer tx.Rollback(ctx)
 
-	_, err := s.db.Exec(ctx, "")
+	var inventoryId int64
+	err = tx.QueryRow(ctx, `INSERT INTO inventory(name, quantity, unit) VALUES ($1, $2, $3) RETURNING id`, data.Name, data.Quantity, data.Unit).Scan(&inventoryId)
 	if err != nil {
-		return 0, fmt.Errorf("cannot insert order: %v", err)
+		return 0, fmt.Errorf("cannot insert into inventory: %v", err)
 	}
 
+	err = tx.Commit(ctx)
+	if err != nil {
+		return 0, fmt.Errorf("cannot commit transaction: %v", err)
+	}
+	s.logr.Info("commit transaction")
+	return inventoryId, nil
+}
+
+func (s *Storage) SaveMenu(ctx context.Context, data models.MenuItem) (int64, error) {
+	tx, err := s.db.Begin(ctx)
+	if err != nil {
+		return 0, fmt.Errorf("cannot begin transaction: %w", err)
+	}
+	defer tx.Rollback(ctx)
+
+	var menuID int64
+	err = tx.QueryRow(ctx, `
+        INSERT INTO menus(name, description, price) 
+        VALUES ($1, $2, $3) 
+        RETURNING id
+    `, data.Name, data.Description, data.Price).Scan(&menuID)
+
+	if err != nil {
+		return 0, fmt.Errorf("cannot save menu: %w", err)
+	}
+
+	for _, ingredient := range data.Ingredients {
+		_, err = tx.Exec(ctx, `INSERT INTO menu_ingredients(ingredient_id, quantity) VALUES ($1, $2, $3)`, ingredient.IngredientID, ingredient.Quantity)
+		if err != nil {
+			return 0, fmt.Errorf("cannot save menu_ingredients: %w", err)
+		}
+	}
+
+	if err = tx.Commit(ctx); err != nil {
+		return 0, fmt.Errorf("cannot commit transaction: %w", err)
+	}
+	return menuID, nil
+}
+
+func (s *Storage) SaveOrder(ctx context.Context, data models.OrderRequest) (int64, error) {
+
+	tx, err := s.db.Begin(ctx)
+	if err != nil {
+		return 0, fmt.Errorf("cannot begin transaction: %w", err)
+	}
+	defer tx.Rollback(ctx)
+
+	var orderId int64
+	err = s.db.QueryRow(ctx, `INSERT INTO orders(
+                   customer_name) VALUES ($1) RETURNING id`, data.CustomerName).Scan(&orderId)
+	if err != nil {
+		return 0, fmt.Errorf("cannot insert into orders: %v", err)
+	}
+
+	for _, items := range data.Items {
+		_, err = s.db.Exec(ctx, `INSERT INTO items(
+                  order_id, menu_id ,quantity) VALUES ($1, $2, $3)`, orderId, items.ProductID, items.Quantity)
+		if err != nil {
+			return 0, fmt.Errorf("cannot insert into items: %v", err)
+		}
+	}
+
+	if err = tx.Commit(ctx); err != nil {
+		return 0, fmt.Errorf("cannot commit transaction: %w", err)
+	}
+	s.logr.Info("order saved")
+
+	return orderId, nil
 }
--- migrations/01_init.up.sql
+++ migrations/01_init.up.sql
@@ -1,41 +1,41 @@
-CREATE TABLE IF NOT EXISTS ingredients (
-    id INT PRIMARY KEY,
-    name TEXT NOT NULL UNIQUE
-);
-
-CREATE TABLE IF NOT EXISTS inventory_items (
-    ingredient_id INT PRIMARY KEY REFERENCES ingredients(id) ON DELETE RESTRICT ,
-    quantity NUMERIC NOT NULL DEFAULT 0,
-    unit TEXT NOT NULL
-);
-
-
-CREATE TABLE IF NOT EXISTS menu_items (
-    id INT PRIMARY KEY,
-    name TEXT NOT NULL,
-    description TEXT NOT NULL,
-    PRICE NUMERIC NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS menu_items_ingredients(
-    menu_item_id INT NOT NULL REFERENCES menu_items(id) ON DELETE CASCADE,
-    ingredient_id INT NOT NULL REFERENCES ingredients(id) ON DELETE RESTRICT,
-    quantity NUMERIC NOT NULL,
-    PRIMARY KEY (menu_item_id, ingredient_id)
-);
-
-
-
 CREATE TABLE IF NOT EXISTS orders (
-    id INT PRIMARY KEY,
-    customer_name TEXT,
-    status TEXT NOT NULL,
-    created_at TIMESTAMP WITH TIME ZONE DEFAULT now()
+    id SERIAL PRIMARY KEY,
+    customer_name TEXT NOT NULL,
+    status TEXT NOT NULL DEFAULT 'open',
+    created_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
 CREATE TABLE IF NOT EXISTS order_items (
-    order_item_id INT NOT NULL REFERENCES orders(id) ON DELETE CASCADE,
-    menu_item_id INT NOT NULL REFERENCES menu_items(id),
-    quantity INT NOT NULL CHECK (quantity > 0),
-    PRIMARY KEY (order_item_id, menu_item_id)
+    id SERIAL PRIMARY KEY,
+    order_id INT NOT NULL REFERENCES orders(id) ON DELETE CASCADE,
+    menu_id INT NOT NULL REFERENCES menus(id) ON DELETE RESTRICT,
+    quantity INT NOT NULL CHECK (quantity > 0)
+);
+
+CREATE TABLE IF NOT EXISTS menus (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL UNIQUE,
+    description TEXT NOT NULL,
+    price NUMERIC NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS ingredients (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL UNIQUE,
+    unit TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS menu_ingredients (
+    menu_id INT NOT NULL REFERENCES menus(id) ON DELETE CASCADE,
+    ingredient_id INT NOT NULL REFERENCES ingredients(id) ON DELETE RESTRICT,
+    quantity NUMERIC NOT NULL CHECK (quantity > 0),
+    PRIMARY KEY (menu_id, ingredient_id)
+);
+
+CREATE TABLE IF NOT EXISTS inventory (
+    id SERIAL PRIMARY KEY,
+    ingredient_id INT NOT NULL REFERENCES ingredients(id) ON DELETE RESTRICT,
+    quantity NUMERIC NOT NULL,
+    unit TEXT NOT NULL
+
 );
